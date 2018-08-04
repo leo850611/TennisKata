cc43a4383fbd34a1896b3cc27c1443100809bbcf
LeoSheu <leo850611@hotmail.com>
2018-08-04 21:42:54 +0800

    thirty love pass

--- TennisKata/Tennis/TennisGame.cs
+++ TennisKata/Tennis/TennisGame.cs
@@ -6,22 +6,26 @@ using System.Threading.Tasks;
 
 namespace Tennis
 {
-    public class TennisGame
-    {
-	    private int _firstPlayerScore;
+	public class TennisGame
+	{
+		private int _firstPlayerScore;
 
-	    public string GetScore()
-	    {
-		    if (_firstPlayerScore == 1)
-		    {
-			    return "Fifteen_Love";
-		    }
-		    return "Love_All";
-	    }
+		public string GetScore()
+		{
+			if (_firstPlayerScore == 1)
+			{
+				return "Fifteen_Love";
+			}
+			else if (_firstPlayerScore == 2)
+			{
+				return "Thirty_Love";
+			}
+			return "Love_All";
+		}
 
-	    public void FirstPlayerGotScore()
-	    {
-		    _firstPlayerScore += 1;
-	    }
-    }
+		public void FirstPlayerGotScore()
+		{
+			_firstPlayerScore += 1;
+		}
+	}
 }
