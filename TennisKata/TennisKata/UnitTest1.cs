8b3f2b8714225ad182996f23980b352405401a11
LeoSheu <leo850611@hotmail.com>
2018-08-04 21:25:21 +0800

    refactor tennis game field

--- TennisKata/TennisKata/UnitTest1.cs
+++ TennisKata/TennisKata/UnitTest1.cs
@@ -6,21 +6,22 @@ namespace TennisKata
 	[TestClass]
 	public class UnitTest1
 	{
+		private TennisGame _tennisGame = new TennisGame();
+
 		[TestMethod]
 		public void LoveAll()
 		{
-			var tennisGame = new TennisGame();
-			var score = tennisGame.GetScore();
+			var score = _tennisGame.GetScore();
 			Assert.AreEqual("Love_All", score);
 		}
 
 		[TestMethod]
 		public void FifteenLove()
 		{
-			var tennisGame = new TennisGame();
-			tennisGame.FirstPlayerGotScore();
-			var score = tennisGame.GetScore();
+			_tennisGame.FirstPlayerGotScore();
+			var score = _tennisGame.GetScore();
 			Assert.AreEqual("Fifteen_Love", score);
 		}
+
 	}
 }
